4c5fbcf665c5403a001442f2e096e6fa91cf74e3
Alejandro López Labajo <mainusone1@gmail.com>
2023-10-27 14:21:03 +0200

    27/10/2023 (3)

--- FRONT/VUE/primervue/src/App.vue
+++ FRONT/VUE/primervue/src/App.vue
@@ -2,17 +2,26 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <HolaMundo />
   <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HomeComponent />
+  <CineComponent />
+  <MusicaComponent />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import HolaMundo from './components/HolaMundo.vue';
+import HomeComponent from './components/HomeComponent.vue';
+import CineComponent from './components/CineComponent.vue';
+import MusicaComponent from './components/MusicaComponent.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    HolaMundo
+    HolaMundo,
+    HomeComponent,
+    CineComponent,
+    MusicaComponent
   }
 }
 </script>
---
+++ FRONT/VUE/primervue/src/components/MusicaComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Musica</h1>
+        <img src="../assets/images/musica.jpg" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MusicaComponent"
+}
+</script>
+
+<style></style>
