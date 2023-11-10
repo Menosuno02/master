1476271f3dcc6ebf3cf058a4d44d43636509e216
Menosuno02 <mainusone1@gmail.com>
2023-11-10 22:37:48 +0100

    Nuxt

--- FRONT/NUXT/nuxtprueba/pages/mapas/index.vue
+++ FRONT/NUXT/nuxtprueba/pages/mapas/index.vue
@@ -1,35 +1,63 @@
 <script setup>
 import Global from "~/Global";
+let mapaElegido = ref(null);
 const { data: mapas } = await useFetch(Global.urlApi + "maps");
 const mapasFiltrar = ["District", "Kasbah", "Piazza", "The Range"];
+
+async function changeMapa(uuid) {
+  await useFetch(Global.urlApi + "maps/" + uuid).then((response) => {
+    mapaElegido.value = response.data._rawValue.data;
+  });
+}
 </script>
 
 <template>
   <div>
     <h1>Mapas</h1>
     <hr class="border border-danger opacity-100" />
-    <table class="table table-bordered border-danger">
-      <thead>
-        <tr>
-          <th></th>
-          <th>Mapa</th>
-          <th>Descripción</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="mapa in mapas.data.filter(
-            (mapa) => !mapasFiltrar.includes(mapa.displayName)
-          )"
-          :key="mapa"
+    <div class="row mb-3">
+      <div class="col-5">
+        <div
+          class="btn-group-vertical w-100"
+          role="group"
+          aria-label="Vertical radio toggle button group"
         >
-          <td class="w-auto p-0">
-            <NuxtImg :src="mapa.splash" class="w-100" />
-          </td>
-          <td class="w-25">{{ mapa.displayName }}</td>
-          <td class="w-auto">{{ mapa.narrativeDescription }}</td>
-        </tr>
-      </tbody>
-    </table>
+          <div
+            v-for="mapa in mapas.data.filter(
+              (mapa) => !mapasFiltrar.includes(mapa.displayName)
+            )"
+            :key="mapa"
+            class="btn-group w-100"
+            role="group"
+          >
+            <input
+              type="radio"
+              class="btn-check"
+              name="vbtn-radio"
+              :id="'vbtn-radio-' + mapa.uuid"
+              autocomplete="off"
+              width="100"
+              @click="changeMapa(mapa.uuid)"
+            />
+            <label
+              class="btn btn-outline-danger w-100"
+              :for="'vbtn-radio-' + mapa.uuid"
+            >
+              {{ mapa.displayName }}
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="col-7" v-if="mapaElegido">
+        <div class="card">
+          <NuxtImg :src="mapaElegido.splash" class="w-100 card-img-top" />
+          <div class="card-body">
+            <p class="card-text text-center">
+              {{ mapaElegido.narrativeDescription }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
